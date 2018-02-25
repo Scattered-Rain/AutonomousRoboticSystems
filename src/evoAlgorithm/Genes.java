aff8a8d6d89fd24b55312af5db4f9977e766977d
Scattered-Rain <Florian221@gmx.net>
2018-02-25 18:26:16 +0100

    :

--- src/evoAlgorithm/Genes.java
+++ src/evoAlgorithm/Genes.java
@@ -1,8 +1,32 @@
 package evoAlgorithm;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import util.Point;
+
 /** Genotype of an Individual */
 public class Genes{
 	
-	//TODO: Create some appropriate representation of Individuals based on the actual problem
+	/** A List of raw Double values */
+	private List<Double> vals;
+	
+	/** A List of Integers representing the indecis (with modulo) of vals that are added up to produce X */
+	private List<Integer> xVals;
+	/** A List of Integers representing the indecis (with modulo) of vals that are added up to produce Y */
+	private List<Integer> yVals;
+	
+	
+	/** Returns a Point representing the expression of this set of Genes */
+	public Point pheno(){
+		double[] xy = new double[]{0, 0};
+		List<Integer>[] lxy = new List[]{xVals, yVals};
+		for(int c=0; c<lxy.length; c++){
+			for(int c2=0; c2<lxy[c].size(); c2++){
+				xy[c] += vals.get(lxy[c].get(c2) % vals.size());
+			}
+		}
+		return new Point(xy[0], xy[1]);
+	}
 	
 }
